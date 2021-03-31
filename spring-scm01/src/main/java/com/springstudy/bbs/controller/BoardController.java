7ef4d36a79cc8147b473ed88c0a4f97a09b55656
user <user@192.168.0.37>
2021-03-31 16:58:45 +0900

    스프링3

--- spring-scm01/src/main/java/com/springstudy/bbs/controller/BoardController.java
+++ spring-scm01/src/main/java/com/springstudy/bbs/controller/BoardController.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springstudy.bbs.domain.Board;
 import com.springstudy.bbs.service.BoardService;
-
+//윤하
 //안녕하세요
 @Controller
 public class BoardController {	
